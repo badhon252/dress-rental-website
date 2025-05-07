40c470fe91a747ba016128f36948cceba13bb15e
Monir Hossain <monirhrabby.personal@gmail.com>
2025-05-07 21:11:35 +0600

    installed: necessary technologies

---
+++ src/app/(website)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center font-nimbus text-[24px] leading-[28px] tracking-[15%] ">
+      Monir Hossain Rabby
+    </div>
+  );
+}
