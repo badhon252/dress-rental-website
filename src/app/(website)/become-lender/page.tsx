0607ecf56d05b1035290a8074c09cd413d1c11a3
ILhankhondaker <ilhankhondaker@gmail.com>
2025-05-11 09:59:01 +0600

    fix nav

---
+++ src/app/(website)/become-lender/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-screen bg-cover bg-center bg-no-repeat'>
+      becode-lender
+    </div>
+  )
+}
+
+export default page
