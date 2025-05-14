c60ef9c8db7916f06178b00a6df878f945cce78e
ILhankhondaker <ilhankhondaker@gmail.com>
2025-05-14 15:40:12 +0600

    make checkout

--- src/app/(website)/checkout/page.tsx
+++ src/app/(website)/checkout/page.tsx
@@ -1,9 +1,11 @@
+import CheckoutPage from '@/components/checkout/Checkout'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       <h1>Cheeckout</h1>
+      <CheckoutPage/>
     </div>
   )
 }
